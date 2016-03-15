69b44813a338b1d119e0c23911b30b9ff0c32a5f
Brian Chan <toyer889@gmail.com>
2016-03-15 16:22:19 -0400

    Update MainPage.js
    added animation to posters on main page

--- MainPage.js
+++ MainPage.js
@@ -1,8 +1,13 @@
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+$(document).ready(function(){
+
+	var scroll1;
+	$(window).scroll(function(){
+		scroll1 = $(window).scrollTop();
+
+			if(scroll1 > 550){
+				$('.poster').animate({'opacity':'1'});
+		}
+
+	});
+
+});
